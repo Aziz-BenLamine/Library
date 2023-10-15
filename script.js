3c463db13d05a1a3c918197d77be566c27dc255f
Aziz-BenLamine <azizbenlamine@gmail.com>
2023-10-15 11:13:21 +0100

    Add functionalites to buttons

--- script.js
+++ script.js
@@ -1,9 +1,59 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function toggleRead(index) {
+    myLibrary[index].read = !myLibrary[index].read;
+    render();
 }
+
+function render() {
+    let Libraryel = document.querySelector("#library");
+    Libraryel.innerHTML = "";
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book = myLibrary[i];
+        let bookel = document.createElement("div");
+        bookel.setAttribute("class", "book-card");
+        bookel.innerHTML = `<div class="card-header">
+        <h3 class="title">${book.title}</h3>
+        <h5 class="author">by ${book.author}</h5>
+      </div>
+      <div class="card-body">
+        <p>${book.pages} pages</p>
+        <p class="read-status">${book.read ? "Read" : "Not Read Yet"}</p>
+        <button class="remove-btn" onclick="removeBook(${i})">Remove</button>
+        <button class="toggle-read-btn" onclick="toggleRead(${i})">Toggle Read</button>
+      </div>`;
+        Libraryel.appendChild(bookel);
+    }
+}
+
+function removeBook(index){
+    myLibrary.splice(index, 1);
+    render();
+}
+
+function addBookToLibrary(event) {
+    event.preventDefault();
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#pages").value;
+    let read = document.querySelector("#read").checked;
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    render();
+}
+
+let addBookbtn = document.querySelector("#add-book-btn");
+addBookbtn.addEventListener("click", function () {
+    let newBookForm = document.querySelector("#new-book-form");
+    newBookForm.style.display = "block";
+})
+
+document.querySelector("#new-book-form").addEventListener("submit", addBookToLibrary);
